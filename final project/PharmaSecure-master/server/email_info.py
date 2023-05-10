1b2879258a965d9414712908f09f07ac9e0d51d0
AmoghPuranik <108808204+AmoghPuranik@users.noreply.github.com>
2023-05-10 13:36:40 +0530

    Add files via upload

---
+++ final project/PharmaSecure-master/server/email_info.py
@@ -0,0 +1,7 @@
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS= True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER='pharmasecure9@gmail.com'
+EMAIL_HOST_PASSWORD='dynbyhroyqaygywj'
+EMAIL_PORT=587
+EMAIL_USE_SSL=False
